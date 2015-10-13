f1bd713d5580d2ad35dd70bebcfd45bb1298d626
Manoj <manoj.khanwalkar@gmail.com>
2015-10-12 22:33:39 -0400

    added weak reference

--- src/test/java/multi/VClientTester.java
+++ src/test/java/multi/VClientTester.java
@@ -12,26 +12,33 @@ public class VClientTester {
     public static void main(String[] args) throws Exception {
 
         final VClient client = VClientFactory.getInstance().getVClient("tester");
-        ExecutorService service = Executors.newFixedThreadPool(10);
 
+        for (int k=0;k<10;k++) {
+            ExecutorService service = Executors.newFixedThreadPool(10);
 
-        for (int i=0;i<100;i++) {
-            service.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                service.submit(() -> {
 
-                client.send("Hello World from " + Thread.currentThread().getName());
-                System.out.println(client.recv());
+                    client.send("Hello World from " + Thread.currentThread().getName());
+                    System.out.println(client.recv());
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-            });
+                });
+            }
+
+            service.shutdown();
+            service.awaitTermination(10, TimeUnit.DAYS);
+            System.gc();
+
         }
 
-        service.shutdown();
-        service.awaitTermination(10, TimeUnit.DAYS);
+
+        Thread.sleep(100000);
 
     }
 
