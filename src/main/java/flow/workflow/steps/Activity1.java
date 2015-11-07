209b7bea545b11bd93f0a2416fdaecc03f09a490
Manoj <manoj.khanwalkar@gmail.com>
2015-11-06 23:18:14 -0500

    Trial code

--- src/main/java/flow/workflow/steps/Activity1.java
+++ src/main/java/flow/workflow/steps/Activity1.java
@@ -17,18 +17,6 @@ public class Activity1 implements Activity {
 
     @Override
     public void execute(Request req, Response res) {
-        System.out.println(req);
-
-        ClientResponse response = (ClientResponse) res;
-
-        Payload payload = new Payload();
-
-        Adapter adapter1 = AdapterManager.getInstance().getAdapter("APP1~C1");
-        App1Response response1 = (App1Response) adapter1.send(req);
-
-        payload.set("App1Response",response1.getDummy() );
-
-        response.setPayload(payload);
 
     }
 }
---
+++ src/main/java/flow/workflow/steps/Activity2.java
@@ -0,0 +1,34 @@
+package flow.workflow.steps;
+
+import flow.adapter.Adapter;
+import flow.adapter.AdapterManager;
+import flow.adapter.app1.App1Response;
+import flow.workflow.Activity;
+import query.ClientResponse;
+import query.Payload;
+import query.Request;
+import query.Response;
+
+/**
+ * Created by mkhanwalkar on 11/6/15.
+ */
+public class Activity2 implements Activity {
+
+    @Override
+    public void execute(Request req, Response res) {
+        System.out.println(req);
+
+        ClientResponse response = (ClientResponse) res;
+
+        Payload payload = new Payload();
+
+        Adapter adapter1 = AdapterManager.getInstance().getAdapter("APP1~C1");
+        App1Response response1 = (App1Response) adapter1.send(req);
+
+        payload.set("App1Response",response1.getDummy() );
+
+        response.setPayload(payload);
+
+    }
+
+}
